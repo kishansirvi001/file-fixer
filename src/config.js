7f1f8eb872c335350c22201db9cd37cdca224745
kishanchoudharyy <kishanchoudharyyy@gmail.com>
2026-04-04 17:20:44 +0530

    fix backend url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // src/config.js
 
-// Always use environment variable (works for both dev + production)
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+// Use environment variable if available, otherwise fallback to local
+const API_BASE =
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export { API_BASE };
